b525b60157948f5ab9b3825eea9856af3cab80e2
Juan Carlos Perez <Juan Carlos Perez@localhost>
2010-02-05 15:13:44 -0600

    ADempiere GWT Initial Project Settings

---
+++ contributions/e-Evolution/ADempiereGWT/src/main/java/org/eevolution/ui/gwt/client/ADempiereGWT.java
@@ -0,0 +1,57 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * Copyright (C) 2003-2007 e-Evolution,SC. All Rights Reserved.               *
+ * Developer(s): Juan Carlos Perez www.e-evolution.com                        *
+ *****************************************************************************/
+package org.eevolution.ui.gwt.client;
+
+import org.eevolution.ui.gwt.client.component.ConfirmPanel;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * ADempiere GWT ADempiereGWT
+ * @author <a href="mailto:jperezcasanova@gmail.com">Juan Carlos Perez</a>
+ * @version $Id: ADempiereGWT.java, v 1.0 Feb 2, 2010
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class ADempiereGWT implements EntryPoint, ClickHandler {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+
+		ConfirmPanel cp = new ConfirmPanel(true, true, false,false, false, false,false);
+		cp.addClickHanler(this);
+		RootPanel.get().add(cp);
+
+	}
+
+	@Override
+	public void onClick(ClickEvent event) {
+		Button button = (Button)event.getSource();
+		if(button.getText().equals("Ok"))
+			Window.alert("Ok was clicked!!!");
+		else if(button.getText().equals("Cancel"))
+			Window.alert("Cancel was clicked!!!");
+		else if(button.getText().equals("Help"))
+			Window.alert("Help was clicked!!!");
+			
+	}
+}
---
+++ contributions/e-Evolution/ADempiereGWT/src/main/java/org/eevolution/ui/gwt/client/component/ConfirmPanel.java
@@ -0,0 +1,93 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * Copyright (C) 2003-2007 e-Evolution,SC. All Rights Reserved.               *
+ * Developer(s): Juan Carlos Perez www.e-evolution.com                        *
+ *****************************************************************************/
+package org.eevolution.ui.gwt.client.component;
+
+import org.eevolution.ui.gwt.client.widget.AButton;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiConstructor;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * ADempiere GWT ConfirmPanel
+ * @author <a href="mailto:jperezcasanova@gmail.com">Juan Carlos Perez</a>
+ * @version $Id: ConfirmPanel.java, v 1.0 Feb 2, 2010
+ */
+public class ConfirmPanel extends Composite {
+
+	interface ConfirmPanelUiBinder extends UiBinder<Widget, ConfirmPanel> {}
+	private static ConfirmPanelUiBinder uiBinder = GWT.create(ConfirmPanelUiBinder.class);
+
+	@UiField AButton cancelButton;
+	@UiField AButton okButton;
+	
+	@UiField AButton helpButton;
+	@UiField AButton refreshButton;
+	@UiField AButton resetButton;
+	@UiField AButton customizeButton;
+	@UiField AButton historyButton;
+	@UiField AButton zoomButton;
+
+	public ConfirmPanel() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+	
+	/**
+	 * 
+	 * @param withCancelButton
+	 * @param withHelpButton
+	 * @param withRefreshButton
+	 * @param withResetButton
+	 * @param withCustomizeButton
+	 * @param withHistoryButton
+	 * @param withZoomButton
+	 */
+	public @UiConstructor ConfirmPanel(boolean withCancelButton,
+			boolean withHelpButton, 
+			boolean withRefreshButton, 
+			boolean withResetButton,
+			boolean withCustomizeButton, 
+			boolean withHistoryButton, 
+			boolean withZoomButton){
+		this();
+
+		cancelButton.setVisible(withCancelButton);
+		helpButton.setVisible(withHelpButton);
+		refreshButton.setVisible(withRefreshButton);
+		resetButton.setVisible(withResetButton);
+		customizeButton.setVisible(withCustomizeButton);
+		historyButton.setVisible(withHistoryButton);
+		zoomButton.setVisible(withZoomButton);
+	}
+	
+
+	public void addClickHanler(ClickHandler  handler){
+		
+		cancelButton.addClickHandler(handler);
+		okButton.addClickHandler(handler);
+		helpButton.addClickHandler(handler);
+		refreshButton.addClickHandler(handler);
+		resetButton.addClickHandler(handler);
+		customizeButton.addClickHandler(handler);
+		historyButton.addClickHandler(handler);
+		zoomButton.addClickHandler(handler);
+		
+	}
+}
