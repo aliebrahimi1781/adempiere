da9529a837b5cd3c80f0cc7e46c2ab0372a216f2
Alberto Juarez <Alberto Juarez@localhost>
2010-03-18 09:06:34 -0600

    Fixed Work Flow Node

--- contributions/e-Evolution/ADempiereGWT/src/main/java/org/eevolution/ui/gwt/client/view/wf/WFNode.java
+++ contributions/e-Evolution/ADempiereGWT/src/main/java/org/eevolution/ui/gwt/client/view/wf/WFNode.java
@@ -10,6 +10,7 @@ import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
+
 /**
  * 
  * ADempiere GWT WFNode
@@ -19,45 +20,73 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class WFNode extends Composite {
 
+
   interface WFNodeUiBinder extends UiBinder<Widget, WFNode> {
   }
-
   private static WFNodeUiBinder uiBinder = GWT.create(WFNodeUiBinder.class);
+  
+  class Rectangle{
+	  int x = 0;
+	  int y = 0;
+	  int height = 0;
+	  int width = 0;
+	  public Rectangle(int x, int y, int width, int height)
+	  {
+		  this.x = x;
+		  this.y = y;
+		  this.width = width;
+		  this.height = height;
+	  }
+  }
+
+  
   @UiField Label header;
-  @UiField HorizontalPanel m_bounds;
+  @UiField HorizontalPanel panel;
   @UiField Image icon;
-  private String Id="";
   
+  String Id = "";
+  Rectangle m_bounds = new Rectangle(0,0,100,50);
   
-  public WFNode(String Id) { //TODO: build with MWFNode
+  public WFNode() { //TODO: build with MWFNode
 	  
-	this.Id=Id;
+
 	initWidget(uiBinder.createAndBindUi(this));
     header.setText("WFNode");
-    m_bounds.setHeight("50px");
-    m_bounds.setWidth("100px");
+    panel.setHeight("50px");
+    panel.setWidth("100px");
   }
 
   public Widget getDrager() {
     return header;
   }
   
-  public String getId(){
-	  return Id;
+  public void setBounds(int x, int y, int width, int height) {
+	  	m_bounds = new Rectangle(x, y, width, height);
+		panel.setWidth(width + "px");
+		panel.setHeight(height + "px");
   }
   
-  public void setBounds(int width, int height) {
-		m_bounds.setWidth(width + "px");
-		m_bounds.setHeight(height + "px");
-  }
+public Rectangle getBounds() {
+	return m_bounds;
+}
   
   public void setToolTip(String toolTip)
   {
-	  m_bounds.setTitle(toolTip);
+	  panel.setTitle(toolTip);
   }
   
   public int getAD_WF_Node_ID() {
 		return 0; //TODO: return Node ID
+  }
+  
+  public void setId(String Id)
+  {
+	  this.Id = Id;
+  }
+  
+  public String getId()
+  {
+	  return Id;
   }
   
   public void getNode() {
